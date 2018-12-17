e0f18f08584729330d273303d1ca717bec8804ab
Liliya Zavatskaya <liliyazavatskaya@MacBook-Pro-Liliya.local>
2018-12-17 17:39:11 +0600

    any anyobject ex

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -82,6 +82,32 @@ var badDog = NoisyDog()
 badDog.bark()
 
 
+var things = [Any]()
+things.append(0)
+things.append(0.0)
+things.append(42)
+things.append("hello")
+things.append((3.0, 5.0))
+things.append({ (name: String) -> String in "Hello, \(name)" })
+
+for thing in things {
+    switch thing {
+        case let someInt as Int:
+            print("an integer value of \(someInt)")
+        case let someDouble as Double where someDouble > 0:
+            print("a positive double value of \(someDouble)")
+        case let someString as String:
+            print("a string value of \" \(someString)\" ")
+        case let (x, y) as (Double, Double):
+            print("an (x, y) point at \(x), \(y)")
+        case let stringConverter as (String) -> String:
+            print(stringConverter("Troll"))
+        default:
+            print("something else")
+    }
+}
+
+
 /*https://www.codewars.com/kata/52fb87703c1351ebd200081f
 func whatCentury(_ year: String) -> String {
     var y = Double(year)!
