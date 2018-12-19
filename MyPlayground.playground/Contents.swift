d84587179e4fa0dace68514f61bcb1ac30ba2224
Liliya Zavatskaya <liliyazavatskaya@MacBook-Pro-Liliya.local>
2018-12-19 08:39:46 +0600

    extension - 1

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -186,6 +186,19 @@ var a = subclass(isNil: true)
 a = nil
 
 
+extension Double {
+    var km: Double {return self * 1000.0}
+    var m: Double {return self}
+    var cm: Double {return self / 100.0}
+    var mm: Double {return self / 1000.0}
+    var ft: Double {return self / 3.28084}
+}
+
+let oneInch = 25.4.mm
+print("один фут это \(oneInch) метра")
+let threeFeet = 3.ft
+print("3 футра это \(threeFeet) метра")
+
 
 /*https://www.codewars.com/kata/52fb87703c1351ebd200081f
 func whatCentury(_ year: String) -> String {
