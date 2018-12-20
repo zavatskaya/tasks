17b5e536bfd7fd1674eac8a493d0dc0144415c2c
Liliya Zavatskaya <liliyazavatskaya@MacBook-Pro-Liliya.local>
2018-12-20 17:00:20 +0600

    ball tasks

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,5 +1,84 @@
 import UIKit
 
+
+import Foundation
+import UIKit
+
+public class Balls: UIView {
+    
+    private var colors: [UIColor]
+    private var balls: [UIView] = []
+    private var ballSize: CGSize = CGSize(width: 40, height: 40)
+    private var animator: UIDynamicAnimator?
+    private var snapBehavior: UISnapBehavior?
+    private var collisionBehavior: UICollisionBehavior
+    
+    public init (colors: [UIColor]) {
+        self.colors = colors
+        collisionBehavior = UICollisionBehavior(items: [])
+        collisionBehavior.setTranslatesReferenceBoundsIntoBoundary(with: UIEdgeInsets(top: 1, left: 1, bottom: 1, right: 1))
+        super.init(frame: CGRect(x: 0, y: 0, width: 400, height: 400))
+        backgroundColor = UIColor.black
+        animator = UIDynamicAnimator(referenceView: self)
+        animator?.addBehavior(collisionBehavior)
+        ballsView()
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func ballsView() {
+        for (index, color) in colors.enumerated() {
+            let ball = UIView(frame: CGRect.zero)
+            ball.backgroundColor = color
+            addSubview(ball)
+            balls.append(ball)
+            let origin = 60 * index + 30
+            ball.frame = CGRect(x: origin, y: origin, width: Int(ballSize.width), height: Int(ballSize.height))
+            ball.layer.cornerRadius = ball.bounds.width / 2.0
+            collisionBehavior.addItem(ball)
+        }
+    }
+    
+    override public func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            let touchLocation = touch.location(in: self)
+            for ball in balls {
+                if (ball.frame.contains(touchLocation)) {
+                    snapBehavior = UISnapBehavior(item: ball, snapTo: touchLocation)
+                    snapBehavior?.damping = 0.5
+                    animator?.addBehavior(snapBehavior!)
+                }
+            }
+        }
+    }
+    
+    override public func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            let touchLocation = touch.location(in: self)
+            if let snapBehavior = snapBehavior {
+                snapBehavior.snapPoint = touchLocation
+            }
+        }
+    }
+    
+    override public func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let snapBehavior = snapBehavior {
+            animator?.removeBehavior(snapBehavior)
+        }
+        snapBehavior = nil
+    }
+    
+    
+}
+
+
+
+
+
+
+/*
 struct Circle {
     var coordinates: (x: Int, y: Int)
     var radius: Float {
@@ -265,6 +344,7 @@ let person = Person(name: "Катя", age: 20)
 happyBDay(celebrator: person)
 
 
+var str: String*/
 
 
 /*https://www.codewars.com/kata/52fb87703c1351ebd200081f
